cf20434fe9f34ad90a77334e726a416bc8fde5eb
tuncbb <mrkite10@gmail.com>
2023-12-02 16:19:25 +0300

    initial commit

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
-from discord.ext import commands
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -7,6 +11,20 @@ intents.guilds = True
 intents.members = True
 
 client = discord.Client(intents=intents)
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GUILD_ID = int(os.getenv("GUILD_ID"))
+MEMBER_CHANNEL_ID = int(os.getenv("MEMBER_CHANNEL_ID"))
+SUCCESS_CHANNEL_ID = int(os.getenv("SUCCESS_CHANNEL_ID"))
+
+def log_message(user_id, message_content, attachment_url=None):
+    with open("message_log.txt", "a") as log_file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"{timestamp} - User {user_id}: {message_content}"
+        if attachment_url:
+            log_entry += f" [Attachment: {attachment_url}]"
+        log_entry += "\n"
+        log_file.write(log_entry)
 
 @client.event
 async def on_ready():
@@ -18,18 +36,23 @@ async def on_message(message):
         return
 
     if isinstance(message.channel, discord.DMChannel):
-        # Replace 'YOUR_GUILD_ID' with your server's ID
-        guild = client.get_guild(YOUR_GUILD_ID)
-        # Replace 'SPECIFIC_CHANNEL_ID' with the ID of the channel you want to check
-        specific_channel = guild.get_channel(SPECIFIC_CHANNEL_ID)
-        # Replace 'ANOTHER_CHANNEL_ID' with the ID of the channel where you want to send the message
-        another_channel = guild.get_channel(ANOTHER_CHANNEL_ID)
+        guild = client.get_guild(GUILD_ID)
+        specific_channel = guild.get_channel(MEMBER_CHANNEL_ID)
+        another_channel = guild.get_channel(SUCCESS_CHANNEL_ID)
 
-        # Check if the user is in the specific channel
         if message.author in specific_channel.members:
-            await another_channel.send(f'{message.author.name} sent: {message.content}')
-            # If the message has attachments (images), send them as well
-            for attachment in message.attachments:
-                await another_channel.send(attachment.url)
+            embed = discord.Embed(title="Anon Success Posted:", description=message.content, color=0x00ff00)
+            attachment_url = None
+            if message.attachments:
+                attachment_url = message.attachments[0].url  # Assuming first attachment is the image
+                embed.set_image(url=attachment_url)
+            await another_channel.send(embed=embed)
+            await message.author.send("Your message has been successfully forwarded!")
 
-client.run('YOUR_BOT_TOKEN')
+            # Log the message and attachment URL
+            log_message(message.author.id, message.content, attachment_url)
+
+        else:
+            await message.author.send("You are not in the required channel to perform this action.")
+
+client.run(BOT_TOKEN)
